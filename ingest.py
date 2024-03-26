8f64ef8004b9fec6cebe668fca3b4996e33e65a2
Avinash <164485189+123456Prad@users.noreply.github.com>
2024-03-26 16:47:08 +0530

    Adding files for test
    Added

---
+++ ingest.py
@@ -0,0 +1,39 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader, PDFMinerLoader,PyPDFDirectoryLoader,Docx2txtLoader,TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import OpenAIEmbeddings
+
+
+print("done")
+
+import os
+from dotenv import load_dotenv
+
+#
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+persist_directory = "./pages/db"
+
+def main():
+    pdf_loader = DirectoryLoader('pages/Reports/Pdf/', glob="**/*.pdf")
+    txt_loader = DirectoryLoader('pages/Reports/txt/', glob="**/*.txt")
+    word_loader = DirectoryLoader('pages/Reports/doc/', glob="**/*.docx")
+
+    loaders = [pdf_loader, txt_loader, word_loader]
+    documents = []
+    for loader in loaders:
+        documents.extend(loader.load())
+
+    print(f"Total number of documents: {len(documents)}")
+    embeddings = OpenAIEmbeddings()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=10)
+    documents = text_splitter.split_documents(documents)
+    db = Chroma.from_documents(documents, embeddings, persist_directory=persist_directory)
+    db.persist()
+    db = None
+
+if __name__ == "__main__":
+    main()
+ 
